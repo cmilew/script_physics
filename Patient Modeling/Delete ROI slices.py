1d76e338a3bb5815f18a67ce00bb36f35c32fadb
Candice Milewski <milewskicandice@gmail.com>
2023-01-25 16:18:30 +0100

    Bugfix pour les ROIs fragmentées

--- Patient Modeling/Delete ROI slices.py
+++ Patient Modeling/Delete ROI slices.py
@@ -2,6 +2,7 @@ from connect import *
 from tkinter import *
 import sys
 
+
 def checks_quit_script():
     """ Function aiming to close any pop-up when the red-cross is clicked """
     global quit_script
@@ -17,14 +18,29 @@ def delete_slices(master, rois_list_names, rois_dict, rois_geom):
     for roi_name in rois_list_names:
         if rois_dict[roi_name].get():
             print(f'Cutting slice of ROI {roi_name}')
-            cut_roi_contours = []
             roi_contours = rois_geom[roi_name].PrimaryShape.Contours
-            # Cutting 3 slices every 4 slice except last slice (to avoid duplicates)
-            for index in range(0, len(roi_contours) - 2, 4):
-                cut_roi_contours.append(roi_contours[index])
-            # Adds last slice of the ROI to be cut (should always be kept)
-            cut_roi_contours.append(roi_contours[len(roi_contours)-1])
-            rois_geom[roi_name].PrimaryShape.Contours = cut_roi_contours
+            # Contours are stored in a list (z) of lists of dictionnaries (containing x, y, z)
+            # If contours are fragmented (several pieces on the same slice) the fragments are stored in different index
+            # of the first list 
+            # e.g. roi_contours[0] and roi_contours[1] have the same z because there are fragments of contours on the
+            # same slice
+            # To begin we retrieve all the z coordinates (without duplicates) that have contours of the ROI considered
+
+            # If the ROI is only on 3 slices => not cuts
+            unique_z_coordinates = sorted(
+                set([coord['z'] for z_index_coord in roi_contours for coord in z_index_coord]))
+            if len(unique_z_coordinates) <= 3:
+                cut_contours = roi_contours
+            else:
+                print("type de unique_z_coordinates")
+                print(type(unique_z_coordinates))
+                # [:len(unique_z_coordinates) - 1:4] : starts at 0 and goes until last by step of 4
+                # [unique_z_coordinates[-1] adds the last slice because we want to keep 1st and last slice contours
+                cut_z_coordinates = unique_z_coordinates[:len(unique_z_coordinates) - 1:4] + [unique_z_coordinates[-1]]
+                # z_index_coord[0]['z'] : 1st index is 0 because every dictionary in z_index_coord has the same z
+                cut_contours = [z_index_coord for z_index_coord in roi_contours if
+                                z_index_coord[0]['z'] in cut_z_coordinates]
+            rois_geom[roi_name].PrimaryShape.Contours = cut_contours
     quit_script = False
     master.destroy()
 
@@ -33,8 +49,6 @@ case = get_current('Case')
 exam = get_current('Examination')
 rois_geom = case.PatientModel.StructureSets[exam.Name].RoiGeometries
 roi_list = [roi.OfRoi.Name for roi in rois_geom if roi.HasContours()]
-print('roi list :')
-print(roi_list)
 rois_list_names = sorted(roi_list, key=lambda string: string.lower())
 
 ### Creation of pop up asking which rois to delete to user ###
@@ -47,22 +61,26 @@ root_pop_up = Tk()
 rois_dict = {roi_name: IntVar() for roi_name in rois_list_names}
 
 root_pop_up.title("Suppression de coupes dans une ROI")
-Label(root_pop_up, text="Choisir les ROIs pour lesquelles il faut supprimer des coupes :").grid(row=0, column=0, 
+Label(root_pop_up, text="Choisir les ROIs pour lesquelles il faut supprimer des coupes :").grid(row=0, column=0,
                                                                                                 padx=10, pady=10)
-# Creates a checkbutton for each empty rois names and every 5 empty rois names, changes the column to have a nicer layout
+# Creates a checkbutton for each empty rois names and every 5 empty rois names, changes the column to have a nicer 
+# layout
 max_lines_per_column = 5
 for r, roi_name in enumerate(rois_list_names):
     check_button = Checkbutton(root_pop_up, text=roi_name, variable=rois_dict[roi_name])
     check_button.grid(row=r % max_lines_per_column + 1, column=r // max_lines_per_column, padx=10, sticky=W)
+
 
 # Adds buttons that selects/unselects all checkboxes
 def select_all_checkboxes():
     for checkbox in rois_dict.values():
         checkbox.set(True)
 
+
 def unselect_all_checkboxes():
     for checkbox in rois_dict.values():
         checkbox.set(False)
+
 
 Button(root_pop_up, text='Tout sélectionner', command=select_all_checkboxes).grid(
     row=max_lines_per_column + 2, column=0, sticky=W, padx=20, pady=1)
